15a6e529963f2485a1c2c74508d67c4a0eef22ad
ty2u <admin@ty2u.com>
2012-06-08 11:39:01 -0400

    Formatting

--- src/me/cakenggt/GeometricMagic/GeometricMagicDamageListener.java
+++ src/me/cakenggt/GeometricMagic/GeometricMagicDamageListener.java
@@ -1,6 +1,5 @@
 package me.cakenggt.GeometricMagic;
 
-
 import org.bukkit.Material;
 import org.bukkit.entity.*;
 //import org.bukkit.event.Event;
@@ -9,28 +8,29 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
-
-
 public class GeometricMagicDamageListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onEntityDamage(EntityDamageEvent event) {
-		//System.out.println("entity damage event");
+		// System.out.println("entity damage event");
 		Entity entity = event.getEntity();
 		int damage = event.getDamage();
-		if (entity instanceof Player){
-			//System.out.println("instance of player");
+		if (entity instanceof Player) {
+			// System.out.println("instance of player");
 			Player player = (Player) entity;
 			int health = player.getHealth();
-			while (player.getInventory().contains(Material.FIRE)){
-				//System.out.println("contains fire");
-				if (health - damage <= 0){
+			while (player.getInventory().contains(Material.FIRE)) {
+				// System.out.println("contains fire");
+				if (health - damage <= 0) {
 					boolean removedFire = false;
-					for (int i = 0; i < player.getInventory().getSize() && !removedFire; i++ ){
-						if (player.getInventory().getItem(i).getType() == Material.FIRE){
-							//System.out.println("removed a fire");
-							int amount = player.getInventory().getItem(i).getAmount();
-							player.getInventory().getItem(i).setAmount(amount - 1);
-							if (amount-1 <= 0){
+					for (int i = 0; i < player.getInventory().getSize()
+							&& !removedFire; i++) {
+						if (player.getInventory().getItem(i).getType() == Material.FIRE) {
+							// System.out.println("removed a fire");
+							int amount = player.getInventory().getItem(i)
+									.getAmount();
+							player.getInventory().getItem(i)
+									.setAmount(amount - 1);
+							if (amount - 1 <= 0) {
 								player.getInventory().clear(i);
 							}
 							damage = damage - health;
@@ -39,8 +39,7 @@ public class GeometricMagicDamageListener implements Listener {
 							removedFire = true;
 						}
 					}
-				}
-				else{
+				} else {
 					event.setDamage(damage);
 					return;
 				}
--- src/me/cakenggt/GeometricMagic/GeometricMagicPlayerDeathListener.java
+++ src/me/cakenggt/GeometricMagic/GeometricMagicPlayerDeathListener.java
@@ -1,6 +1,5 @@
 package me.cakenggt.GeometricMagic;
 
-
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,36 +9,32 @@ import org.bukkit.event.entity.EntityDeathEvent;
 //import org.bukkit.event.player.PlayerListener;
 import org.bukkit.inventory.ItemStack;
 
-
-
 public class GeometricMagicPlayerDeathListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerDeath(EntityDeathEvent event) {
-		if (event.getEntity() instanceof Player){
+		if (event.getEntity() instanceof Player) {
 			Player player = (Player) event.getEntity();
 			int level = player.getLevel();
-			while (level > 5){
-				if (level > 13824){
-					ItemStack oneDiamondBlock = new ItemStack(Material.DIAMOND_BLOCK, 1);
-					player.getWorld().dropItem(player.getLocation(), oneDiamondBlock);
+			while (level > 5) {
+				if (level > 13824) {
+					ItemStack oneDiamondBlock = new ItemStack(
+							Material.DIAMOND_BLOCK, 1);
+					player.getWorld().dropItem(player.getLocation(),
+							oneDiamondBlock);
 					level -= 13824;
-				}
-				else if (level > 1568){
+				} else if (level > 1568) {
 					ItemStack one84 = new ItemStack(84, 1);
 					player.getWorld().dropItem(player.getLocation(), one84);
 					level -= 1568;
-				}
-				else if (level > 153){
+				} else if (level > 153) {
 					ItemStack one29 = new ItemStack(29, 1);
 					player.getWorld().dropItem(player.getLocation(), one29);
 					level -= 153;
-				}
-				else if (level > 12){
+				} else if (level > 12) {
 					ItemStack one70 = new ItemStack(70, 1);
 					player.getWorld().dropItem(player.getLocation(), one70);
 					level -= 12;
-				}
-				else if (level > 1){
+				} else if (level > 1) {
 					ItemStack one2 = new ItemStack(2, 1);
 					player.getWorld().dropItem(player.getLocation(), one2);
 					level -= 1;
