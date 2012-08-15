f07017f8af7f47677e90d5331eb2bc6938439be0
Andrew Stevanus <hoot893@gmail.com>
2012-08-15 17:52:10 -0400

    Minor refactor

--- src/me/cakenggt/GeometricMagic/GeometricMagicPlayerListener.java
+++ src/me/cakenggt/GeometricMagic/GeometricMagicPlayerListener.java
@@ -693,8 +693,7 @@ public class GeometricMagicPlayerListener implements Listener {
 		Block effectBlock = actBlock;
 
 		List<Entity> entitiesList = player.getNearbyEntities(242, 20, 242);
-		String limitArrowsConfig = plugin.getConfig().getString("setcircles.limitarrows").toString();
-		int limitArrows = Integer.parseInt(limitArrowsConfig);
+		int limitArrows = plugin.getConfig().getInt("setcircles.limitarrows");
 		int limit = (entitiesList.size() > limitArrows) ? limitArrows : entitiesList.size();
 
 		for (int i = 0; i < limit; i++) {
